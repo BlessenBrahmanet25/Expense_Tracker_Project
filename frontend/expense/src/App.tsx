80ed00fb1345caef9d165b0fcae5d3808d982304
Blessen25 <blessenvinoymathew25@gmail.com>
2025-02-26 10:10:42 +0530

    Commited Changes

--- frontend/expense/src/App.tsx
+++ frontend/expense/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import './sass_styles/app.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomeComp } from './components/homecomp/homecomp';
 
 const App = () => {
   return (
-
-    <h1>Hello World</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <>
+              <HomeComp />
+            </>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
---
+++ frontend/expense/src/components/homecomp/homecomp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homecomp.scss";
+
+const HomeComp = () => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+
+export { HomeComp }
