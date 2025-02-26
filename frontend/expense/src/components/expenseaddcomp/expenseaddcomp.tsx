1ad5cb4af9e2b66567ba67081c8cb86ea740ac36
Blessen25 <blessenvinoymathew25@gmail.com>
2025-02-26 12:32:58 +0530

    Commited Changes

--- frontend/expense/src/components/expenseaddcomp/expenseaddcomp.tsx
+++ frontend/expense/src/components/expenseaddcomp/expenseaddcomp.tsx
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import './expenseaddcomp.scss';
 
+interface BalanceshowingdivProps {
+    main_balance: any;
+}
 export const ExpenseAddComp = () => {
+
+    const [balance, setBalance] = useState(0);
+    const [radiobtnone, setRadiobtnone] = useState(false);
+    const [radiobtntwo, setRadiobtntwo] = useState(false);
+    const Handleradiobtnone = () => {
+        setRadiobtnone(!radiobtnone)
+        setRadiobtntwo(false)
+    }
+
+    const Handleradiobtntwo = () => {
+        setRadiobtntwo(!radiobtntwo)
+        setRadiobtnone(false)
+    }
     return (
         <>
-            <h1>Expense add comp</h1>
+            <div className="expenseparentdiv">
+                <div className="expensechilddiv">
+                    <h1>Expense Tracker</h1>
+                    <BalanceShowingdivcomp main_balance={balance} />
+                    <div className="expenseshowingmaindiv">
+                        <div className="inputtextdivcstm">
+                            <input type="text" placeholder="Enter Label ( like Groceries, Freelancing etc ...) " />
+                            <input type="number" placeholder="Enter Amount" />
+                            <div className="radiobtnscstmdiv">
+                                <div className="radiobtnone">
+                                    <div className={`radioclickone ${radiobtnone ? ("radioactive") : ("")}`} onClick={Handleradiobtnone}></div>
+                                    <p>Expense</p>
+                                </div>
+                                <div className="radiobtnone">
+                                    <div className={`radioclickone ${radiobtntwo ? ("radioactive") : ("")}`} onClick={Handleradiobtntwo}></div>
+                                    <p>Income</p>
+                                </div>
+                            </div>
+                            <div className="buttoncstmaddtransaction">
+                                <a href="#" className="btnaddtransactioncstm">Add Transaction</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
+
+export const BalanceShowingdivcomp: React.FC<BalanceshowingdivProps> = (Props) => {
+
+    return (
+        <>
+            <div className="expensebalancediv">
+                <div className="balancediv">
+                    <h1>Balance : {Props.main_balance}</h1>
+                </div>
+                <a href="#" className="cancelbuttoncstm">Cancel</a>
+            </div>
+        </>
+    )
+
+}
