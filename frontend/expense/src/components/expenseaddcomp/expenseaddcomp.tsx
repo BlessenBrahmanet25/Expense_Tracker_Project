5e57ffee57a788c9d977e03f2aec2e2d655d9627
Blessen25 <blessenvinoymathew25@gmail.com>
2025-02-27 16:04:55 +0530

    Commited Changes

--- frontend/expense/src/components/expenseaddcomp/expenseaddcomp.tsx
+++ frontend/expense/src/components/expenseaddcomp/expenseaddcomp.tsx
@@ -1,23 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './expenseaddcomp.scss';
 
 interface BalanceshowingdivProps {
     main_balance: any;
 }
+
+interface TransactionProps {
+
+    id: number;
+    type: "Expense" | "Income" | " ";
+    amount: number;
+    label: string;
+}
+
+
 export const ExpenseAddComp = () => {
 
     const [balance, setBalance] = useState(0);
     const [radiobtnone, setRadiobtnone] = useState(false);
     const [radiobtntwo, setRadiobtntwo] = useState(false);
     const [checked, setChecked] = useState<string | null>(null);
+    const [amount, setAmount] = useState("")
+    const [label, setLabel] = useState("")
     const [errorTrue, setErrorTrue] = useState(false);
     const [error, setError] = useState('');
+    const [transactions, setTransactions] = useState<TransactionProps[]>([])
+    const [type, setType] = useState<"Expense" | "Income" | " ">(" ");
+    const [expense, setExpense] = useState(0);
+    const [income, setIncome] = useState(0);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const filteredTransactions = transactions.filter((transaction) =>
+        transaction.label.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    useEffect(() => {
+        localStorage.setItem("transactions", JSON.stringify(transactions));
+    }, [transactions]);
+
+    const HandleAddtransaction = () => {
+        setErrorTrue(true)
+        setError("Please select either Expense or Income")
+        if (radiobtnone === true || radiobtntwo === true) {
+            if (!amount || !label) return;
+
+            const newTransaction: TransactionProps = {
+                id: Date.now(),
+                type,
+                amount: parseFloat(amount),
+                label,
+            };
+
+            setTransactions([...transactions, newTransaction]);
+
+            if (type === "Expense") {
+
+                setBalance((prev) => prev - parseFloat(amount));
+                setExpense((prev) => prev + parseFloat(amount))
+            } else {
+                setBalance((prev) => prev + parseFloat(amount));
+                setIncome((prev) => prev + parseFloat(amount))
+            }
+
+            setAmount("");
+            setLabel("");
+        }
+    }
+
+    const HandleRemoveTransaction = (id: number, transactionAmount: number, transactionType: "Expense" | "Income" | " ") => {
+        setTransactions(transactions.filter((t) => t.id !== id))
+
+        if (transactionType === "Expense") {
+            setBalance((prev) => prev + transactionAmount);
+        } else {
+            setBalance((prev) => prev - transactionAmount);
+        }
+    };
 
     const Handleradiobtnone = () => {
         setRadiobtnone(!radiobtnone)
         setRadiobtntwo(false)
         setChecked('Expense')
         setErrorTrue(false)
+        setType("Expense")
     }
 
     const Handleradiobtntwo = () => {
@@ -25,13 +90,9 @@ export const ExpenseAddComp = () => {
         setRadiobtnone(false)
         setChecked('Income')
         setErrorTrue(false)
+        setType("Income")
     }
 
-    const HandleAddtransaction = () => {
-
-        setErrorTrue(true)
-        setError("Please select either Expense or Income")
-    }
     return (
         <>
             <div className="expenseparentdiv">
@@ -40,8 +101,8 @@ export const ExpenseAddComp = () => {
                     <BalanceShowingdivcomp main_balance={balance} />
                     <div className="expenseshowingmaindiv">
                         <div className="inputtextdivcstm">
-                            <input type="text" placeholder="Enter Label ( like Groceries, Freelancing etc ...) " />
-                            <input type="number" placeholder="Enter Amount" />
+                            <input type="text" placeholder="Enter Label ( like Groceries, Freelancing etc ...) " value={label} onChange={(e) => setLabel(e.target.value)} maxLength={14} />
+                            <input type="text" placeholder="Enter Amount" value={amount} onChange={(e) => setAmount(e.target.value)} maxLength={10} />
                             <div className="radiobtnscstmdiv">
                                 <div className="radiobtnone">
                                     <div className={`radioclickone ${radiobtnone ? ("radioactive") : ("")}`} onClick={Handleradiobtnone}></div>
@@ -69,28 +130,37 @@ export const ExpenseAddComp = () => {
                     <div className="expenseandincomediv">
                         <div className="expensecstmdiv">
                             <h1>Expense</h1>
-                            <h2>Rs {balance}</h2>
+                            <h2>Rs {expense}</h2>
                         </div>
                         <div className="expensecstmdiv">
                             <h1>Income</h1>
-                            <h2>Rs {balance}</h2>
+                            <h2>Rs {income}</h2>
                         </div>
                     </div>
                     <div className="Transactionhistory">
                         <h1>Transactions</h1>
                         <div className="searchtransactiondiv">
-                            <input type="Search" className="searchtransactioncstm" placeholder="Search for transactions ..." />
+                            <input type="Search" className="searchtransactioncstm" placeholder="Search for transactions ..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                         </div>
-                        <div className="transactionallhistory borderrightcstm_red">
-                            <p>Label</p>
-                            <p>Amount</p>
-                            <a href="#" className="removecstmbtn">Remove</a>
-                        </div>
-                        <div className="transactionallhistory borderrightcstm_green">
-                            <p>Label</p>
-                            <p>Amount</p>
-                            <a href="#" className="removecstmbtn">Remove</a>
-                        </div>
+                        {
+                            radiobtnone == true || radiobtntwo == true ? (
+                                <>
+                                    {filteredTransactions.length > 0 ?
+                                        (filteredTransactions.map((items) => (
+                                            <div className={`transactionallhistory ${items.type === 'Expense' ? ("borderrightcstm_red") : ("borderrightcstm_green")}`} key={items.id}>
+                                                <p>{items.label}</p>
+                                                <p>{items.amount}</p>
+                                                <a href="#" className="removecstmbtn" onClick={() => HandleRemoveTransaction(items.id, items.amount, items.type)}>Remove</a>
+                                            </div>
+                                        ))) : (
+                                            <p className="notransactionfoundcstm">No Transactions Found</p>
+                                        )}
+                                </>
+                            ) : (<>
+                                <p className="notransactionfoundcstm">No Transactions Found</p>
+                            </>
+                            )
+                        }
                     </div>
                 </div>
             </div>
@@ -104,9 +174,9 @@ export const BalanceShowingdivcomp: React.FC<BalanceshowingdivProps> = (Props) =
         <>
             <div className="expensebalancediv">
                 <div className="balancediv">
-                    <h1>Balance : {Props.main_balance}</h1>
+                    <h1>Balance : Rs {Props.main_balance}</h1>
                 </div>
-                <a href="#" className="cancelbuttoncstm">Cancel</a>
+                <a href="/" className="cancelbuttoncstm">Cancel</a>
             </div>
         </>
     )
